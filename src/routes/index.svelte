d0ae3f4de7a47b99a673e511a50719ac04155b27
K-Sato <katsuki_sato0130@icloud.com>
2022-04-30 15:30:55 +0900

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,32 @@
+<script>
+	import CodeFlask from 'codeflask';
+	import { onMount } from 'svelte';
+
+	let editor;
+	let flask;
+	let defaultCode = `const msg = "Hello World"\n\nconst greeting = () => {\n  console.log(msg)\n}\n\ngreeting()\n\n`;
+
+	onMount(() => {
+		const codeFlask = createEditor();
+
+		if (codeFlask) {
+			flask = codeFlask;
+			flask.updateCode(defaultCode);
+		} else {
+			throw new Error('Failed to load the editor');
+		}
+	});
+
+	function createEditor() {
+		if (editor) editor.innerHTML = '';
+
+		const flask = new CodeFlask(editor, {
+			language: 'js',
+			lineNumbers: true
+		});
+
+		return flask;
+	}
+</script>
+
+<div bind:this={editor} />
